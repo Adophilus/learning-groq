5bf9bd729bbeba611816e080b719e3487188c02b
Adophilus <uchenna19of@gmail.com>
2023-06-02 15:10:41 +0100

    updated joining examples

--- src/010-joining.ts
+++ src/010-joining.ts
@@ -3,7 +3,7 @@ import { database, groq } from "./lib";
 groq`*[]{
   first_name,
   last_name,
-  "relatives": *[id != ^.id && last_name == ^.last_name]{ first_name, last_name}
+  "relatives": *[id != ^.id && last_name == ^.last_name]{ first_name, last_name }
 }`
   .then((query) => query(database))
-  .then((relatedPeople) => console.log(relatedPeople));
+  .then((peopleWithRelations) => console.log(peopleWithRelations));
