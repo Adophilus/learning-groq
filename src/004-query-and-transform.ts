2d65af3623fdff3c9a89e39188a613dfc61c59e0
Adophilus <uchenna19of@gmail.com>
2023-06-02 14:17:04 +0100

    added spread example

--- src/004-query-and-transform.ts
+++ src/004-query-and-transform.ts
@@ -7,6 +7,13 @@ groq`*[country == "Nigeria"]{
   .then((nigerianNames) => console.log(nigerianNames));
 
 groq`*[country == "Nigeria"]{
+  "full_name": first_name + " " + last_name,
+  ...
+}`
+  .then((query) => query(database))
+  .then((nigerianNames) => console.log(nigerianNames));
+
+groq`*[country == "Nigeria"]{
   "full_name": first_name + " " + last_name
 }.full_name`
   .then((query) => query(database))
