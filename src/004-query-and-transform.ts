8b0bd402a397bc4c4d300657dae8157607c46851
Adophilus <uchenna19of@gmail.com>
2023-06-02 14:14:27 +0100

    worked on exercise 4

--- src/004-query-and-transform.ts
+++ src/004-query-and-transform.ts
@@ -5,3 +5,9 @@ groq`*[country == "Nigeria"]{
 }`
   .then((query) => query(database))
   .then((nigerianNames) => console.log(nigerianNames));
+
+groq`*[country == "Nigeria"]{
+  "full_name": first_name + " " + last_name
+}.full_name`
+  .then((query) => query(database))
+  .then((nigerianNames) => console.log(nigerianNames));
